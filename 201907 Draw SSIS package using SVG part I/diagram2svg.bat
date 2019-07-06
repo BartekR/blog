a7df673c4129be5e665a52e5d7d19ce324d201f3
Bartosz Ratajczyk <b.ratajczyk@gmail.com>
2019-07-06 13:36:58 +0200

    New version of Sample2.svg (no grid, gray color)

--- 201907 Draw SSIS package using SVG part I/diagram2svg.bat
+++ 201907 Draw SSIS package using SVG part I/diagram2svg.bat
@@ -4,7 +4,7 @@ set SAXONPATH=D:\tools\SaxonHE9.9N\bin
 
 %SAXONPATH%\Transform -s:Beginning.xml -xsl:package2svg.xsl -o:Beginning.svg
 
-rem %SAXONPATH%\Transform -s:Sample2.xml -xsl:package2svg.xsl -o:Sample2.svg
+%SAXONPATH%\Transform -s:Sample2.xml -xsl:package2svg.xsl -o:Sample2.svg
 %SAXONPATH%\Transform -s:01.xml -xsl:01.xsl -o:01.svg
 %SAXONPATH%\Transform -s:02.xml -xsl:02.xsl -o:02.svg
 %SAXONPATH%\Transform -s:02.xml -xsl:02a.xsl -o:02a.svg
