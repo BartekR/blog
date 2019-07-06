0a22b022d190ca74b62061b5b811c22f1ef5af6a
Bartosz Ratajczyk <b.ratajczyk@gmail.com>
2019-07-06 09:01:01 +0200

    Added generated SVG files

--- 201907 Draw SSIS package using SVG part I/diagram2svg.bat
+++ 201907 Draw SSIS package using SVG part I/diagram2svg.bat
@@ -5,7 +5,7 @@ set SAXONPATH=D:\tools\SaxonHE9.9N\bin
 %SAXONPATH%\Transform -s:Beginning.xml -xsl:package2svg.xsl -o:Beginning.svg
 
 rem %SAXONPATH%\Transform -s:Sample2.xml -xsl:package2svg.xsl -o:Sample2.svg
-rem %SAXONPATH%\Transform -s:01.xml -xsl:01.xsl -o:01.svg
-rem %SAXONPATH%\Transform -s:02.xml -xsl:02.xsl -o:02.svg
-rem %SAXONPATH%\Transform -s:03.xml -xsl:03.xsl -o:03.svg
+%SAXONPATH%\Transform -s:01.xml -xsl:01.xsl -o:01.svg
+%SAXONPATH%\Transform -s:02.xml -xsl:02.xsl -o:02.svg
+%SAXONPATH%\Transform -s:03.xml -xsl:03.xsl -o:03.svg
 
